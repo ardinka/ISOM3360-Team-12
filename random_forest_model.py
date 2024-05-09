7f7ca72cca9355fb8e53915e5163fa638b08c5b2
ardinka <hlkchan@connect.ust.hk>
2024-05-09 11:01:02 +0800

    fixed random forest model

--- random_forest_model.py
+++ random_forest_model.py
@@ -20,4 +20,21 @@ def train_random_forest_model(X_train, y_train):
     grid_search = GridSearchCV(estimator=rfm, param_grid=param_grid, cv=5)
     # Train the model with grid search
     grid_search.fit(X_train, y_train)
-    return grid_search
+
+    # Extract the best parameters from the GridSearchCV model
+    best_params = grid_search.best_params_
+
+    # Create the decision tree model using the best parameters
+    random_forest_model = RandomForestClassifier(
+        n_estimators=best_params['n_estimators'],
+        max_depth=best_params['max_depth'],
+        min_samples_split=best_params['min_samples_split'],
+        min_samples_leaf=best_params['min_samples_leaf'],
+        max_features=best_params['max_features'],
+        bootstrap=best_params['bootstrap']
+    )
+
+    # Train the decision tree model
+    random_forest_model.fit(X_train, y_train)
+
+    return random_forest_model
